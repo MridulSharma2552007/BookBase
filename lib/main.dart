995d060dd4d097dc2bacefa87d659c73f7f55b5e
MridulSharma2552007 <mridul25sharma52007@gmail.com>
2025-07-04 19:05:46 +0530

    Supabase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:book_base/partone/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    anonKey: 'https://lauiqjjvmlmheafjhzpq.supabase.co',
+    url:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxhdWlxamp2bWxtaGVhZmpoenBxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE2MzU1ODAsImV4cCI6MjA2NzIxMTU4MH0.5NOmv3zxqaF54uJYIri2LvCYYyHffRLEwGozvfoF1OU',
+  );
+
   runApp(MyApp());
 }
 
--- lib/partone/login_screen.dart
+++ lib/partone/login_screen.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
