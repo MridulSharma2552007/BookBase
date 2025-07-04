cefcd7f628d6ae1bdc466fa79f6e091665f5d825
MridulSharma2552007 <mridul25sharma52007@gmail.com>
2025-07-04 19:15:54 +0530

    login page created

--- lib/partone/login_screen.dart
+++ lib/partone/login_screen.dart
@@ -1,4 +1,6 @@
+import 'package:book_base/colors/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -10,6 +12,20 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: AppColors.primary,
+      appBar: AppBar(
+        backgroundColor: AppColors.primary,
+        title: Text(
+          'Book Base',
+          style: GoogleFonts.plusJakartaSans(
+            color: AppColors.secondary,
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+          ),
+        ),
+        centerTitle: true,
+      ),
+    );
   }
 }
