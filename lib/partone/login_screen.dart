b17e5540419403afab35ace256b6b431b5cac482
MridulSharma2552007 <mridul25sharma52007@gmail.com>
2025-07-05 15:06:53 +0530

    login modified

--- lib/partone/login_screen.dart
+++ lib/partone/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:book_base/colors/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -14,17 +15,67 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.primary,
-      appBar: AppBar(
-        backgroundColor: AppColors.primary,
-        title: Text(
-          'Book Base',
-          style: GoogleFonts.plusJakartaSans(
-            color: AppColors.secondary,
-            fontWeight: FontWeight.bold,
-            fontSize: 24,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset('assets/images/login.png', height: 200),
+                const SizedBox(height: 20),
+                Text(
+                  'Welcome to BookBase',
+                  style: GoogleFonts.plusJakartaSans(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: AppColors.secondary,
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Your personal library awaits',
+                  style: GoogleFonts.plusJakartaSans(
+                    fontSize: 18,
+                    color: AppColors.secondary.withValues(alpha: 0.7),
+                  ),
+                ),
+                SizedBox(height: 40),
+                SizedBox(
+                  width: double.infinity,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 40),
+                    child: ElevatedButton.icon(
+                      icon: FaIcon(
+                        FontAwesomeIcons.google,
+                        color: Colors.white,
+                      ),
+                      onPressed: () {
+                        
+                      },
+                      label: Text(
+                        'Sign In with Google',
+                        style: TextStyle(
+                          color: AppColors.primary,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 16,
+                        ),
+                      ),
+
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: AppColors.secondary,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
-        centerTitle: true,
       ),
     );
   }
