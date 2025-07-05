c3dac417734afd316072ce2db48d8332735ffe3a
MridulSharma2552007 <mridul25sharma52007@gmail.com>
2025-07-05 21:37:20 +0530

    Selection page created

--- lib/partone/selection_screen.dart
+++ lib/partone/selection_screen.dart
@@ -1,6 +1,8 @@
+import 'package:book_base/colors/app_colors.dart';
 import 'package:book_base/partone/login_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class SelectionScreen extends StatefulWidget {
@@ -11,38 +13,28 @@ class SelectionScreen extends StatefulWidget {
 }
 
 class _SelectionScreenState extends State<SelectionScreen> {
-  Future<void> signOut(BuildContext context) async {
-    try {
-      // Sign out from Firebase
-      await FirebaseAuth.instance.signOut();
-
-      // Also sign out from Google (optional but recommended)
-      await GoogleSignIn().signOut();
-
-      // Optional: Navigate back to login screen or show a message
-      ScaffoldMessenger.of(
-        context,
-      ).showSnackBar(SnackBar(content: Text('Signed out successfully')));
-    } catch (e) {
-      print("Sign-out error: $e");
-      ScaffoldMessenger.of(
-        context,
-      ).showSnackBar(SnackBar(content: Text('Sign-out failed: $e')));
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            signOut(context);
-            Navigator.push(context, MaterialPageRoute(
-              builder: (context) => const LoginScreen(),
-            ));
-          },
-          child: const Text('Go to Home'),
+      backgroundColor: AppColors.primary,
+      appBar: AppBar(
+        backgroundColor:AppColors.primary,
+        title: Text(
+          'Select Semester',
+          style: GoogleFonts.plusJakartaSans(
+            color: AppColors.secondary,
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [],
+          ),
         ),
       ),
     );
