be5621ffe609fd079be7b99a45bd19fcf14bd53a
MridulSharma2552007 <mridul25sharma52007@gmail.com>
2025-07-06 21:00:28 +0530

    Selection page shared prefs added

--- lib/partone/selection_screen.dart
+++ lib/partone/selection_screen.dart
@@ -3,6 +3,7 @@
 import 'package:book_base/colors/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SelectionScreen extends StatefulWidget {
   const SelectionScreen({super.key});
@@ -12,6 +13,12 @@ class SelectionScreen extends StatefulWidget {
 }
 
 class _SelectionScreenState extends State<SelectionScreen> {
+  final String sem = '';
+  void storeSem() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('sem', sem);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,7 +42,14 @@ class _SelectionScreenState extends State<SelectionScreen> {
               semContainer(
                 semName: 'Semester 1',
                 semImage: 'assets/semImages/sem_one.png',
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SelectionScreen(),
+                    ),
+                  );
+                },
               ),
               semContainer(
                 semName: 'Semester 2',
