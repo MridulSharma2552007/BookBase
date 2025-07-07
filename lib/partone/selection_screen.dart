d84a7525b86196632ebb79bc29f94b359aa376df
MridulSharma2552007 <mridul25sharma52007@gmail.com>
2025-07-07 21:32:43 +0530

    Passing Sem Name with on tap

--- lib/partone/selection_screen.dart
+++ lib/partone/selection_screen.dart
@@ -6,145 +6,145 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SelectionScreen extends StatefulWidget {
-  const SelectionScreen({super.key});
+const SelectionScreen({super.key});
 
-  @override
-  State<SelectionScreen> createState() => _SelectionScreenState();
+@override
+State<SelectionScreen> createState() => _SelectionScreenState();
 }
 
 class _SelectionScreenState extends State<SelectionScreen> {
-  final String sem = '';
-  void storeSem() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setString('sem', sem);
-  }
+String sem = '';
+void storeSem(String semName) async {
+  sem = semName;
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  prefs.setString('sem', sem);
+}
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    backgroundColor: AppColors.primary,
+    appBar: AppBar(
       backgroundColor: AppColors.primary,
-      appBar: AppBar(
-        backgroundColor: AppColors.primary,
-        title: Text(
-          'Select Semester',
-          style: GoogleFonts.plusJakartaSans(
-            color: AppColors.secondary,
-            fontWeight: FontWeight.bold,
-            fontSize: 24,
-          ),
-        ),
-        centerTitle: true,
-      ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: [
-              semContainer(
-                semName: 'Semester 1',
-                semImage: 'assets/semImages/sem_one.png',
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const SelectionScreen(),
-                    ),
-                  );
-                },
-              ),
-              semContainer(
-                semName: 'Semester 2',
-                semImage: 'assets/semImages/sem_two.png',
-                onPressed: () {},
-              ),
-              semContainer(
-                semName: 'Semester 3',
-                semImage: 'assets/semImages/sem_three.png',
-                onPressed: () {},
-              ),
-              semContainer(
-                semName: 'Semester 4',
-                semImage: 'assets/semImages/sem_four.png',
-                onPressed: () {},
-              ),
-              semContainer(
-                semName: 'Semester 5',
-                semImage: 'assets/semImages/sem_five.png',
-                onPressed: () {},
-              ),
-              semContainer(
-                semName: 'Semester 6',
-                semImage: 'assets/semImages/sem_six.png',
-                onPressed: () {},
-              ),
-              semContainer(
-                semName: 'Semester 7',
-                semImage: 'assets/semImages/sem_seven.png',
-                onPressed: () {},
-              ),
-              semContainer(
-                semName: 'Semester 8',
-                semImage: 'assets/semImages/sem_eight.png',
-                onPressed: () {},
-              ),
-            ],
-          ),
+      title: Text(
+        'Select Semester',
+        style: GoogleFonts.plusJakartaSans(
+          color: AppColors.secondary,
+          fontWeight: FontWeight.bold,
+          fontSize: 24,
         ),
       ),
-    );
-  }
+      centerTitle: true,
+    ),
+    body: SingleChildScrollView(
+      child: Center(
+        child: Column(
+          children: [
+            semContainer(
+              semName: 'Semester 1',
+              semImage: 'assets/semImages/sem_one.png',
+              onPressed: () {
+                storeSem('Semester 1');
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const SelectionScreen(),
+                  ),
+                );
+              },
+            ),
+            semContainer(
+              semName: 'Semester 2',
+              semImage: 'assets/semImages/sem_two.png',
+              onPressed: () {},
+            ),
+            semContainer(
+              semName: 'Semester 3',
+              semImage: 'assets/semImages/sem_three.png',
+              onPressed: () {},
+            ),
+            semContainer(
+              semName: 'Semester 4',
+              semImage: 'assets/semImages/sem_four.png',
+              onPressed: () {},
+            ),
+            semContainer(
+              semName: 'Semester 5',
+              semImage: 'assets/semImages/sem_five.png',
+              onPressed: () {},
+            ),
+            semContainer(
+              semName: 'Semester 6',
+              semImage: 'assets/semImages/sem_six.png',
+              onPressed: () {},
+            ),
+            semContainer(
+              semName: 'Semester 7',
+              semImage: 'assets/semImages/sem_seven.png',
+              onPressed: () {},
+            ),
+            semContainer(
+              semName: 'Semester 8',
+              semImage: 'assets/semImages/sem_eight.png',
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
 }
 
 class semContainer extends StatelessWidget {
-  final String semName;
-  final String semImage;
-  final VoidCallback onPressed;
-  const semContainer({
-    super.key,
-    required this.semName,
-    required this.semImage,
-    required this.onPressed,
-  });
+final String semName;
+final String semImage;
+final VoidCallback onPressed;
+const semContainer({
+  super.key,
+  required this.semName,
+  required this.semImage,
+  required this.onPressed,
+});
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Image.asset(semImage),
+@override
+Widget build(BuildContext context) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.center,
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Image.asset(semImage),
 
-        Padding(
-          padding: const EdgeInsets.all(25),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                semName,
-                style: GoogleFonts.plusJakartaSans(
-                  color: AppColors.secondary,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
+      Padding(
+        padding: const EdgeInsets.all(25),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              semName,
+              style: GoogleFonts.plusJakartaSans(
+                color: AppColors.secondary,
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: onPressed,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
                 ),
               ),
-              ElevatedButton(
-                onPressed: () {
-                  onPressed;
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                ),
-                child: Text(
-                  'Select',
-                  style: GoogleFonts.plusJakartaSans(color: AppColors.primary),
-                ),
+              child: Text(
+                'Select',
+                style: GoogleFonts.plusJakartaSans(color: AppColors.primary),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-      ],
-    );
-  }
+      ),
+    ],
+  );
+}
 }
