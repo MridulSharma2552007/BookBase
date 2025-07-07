0dca0c06520ca4ec5761a604f24ca69296943510
MridulSharma2552007 <mridul25sharma52007@gmail.com>
2025-07-07 22:05:26 +0530

    Selection bar created

--- lib/partone/bookpage.dart
+++ lib/partone/bookpage.dart
@@ -12,6 +12,7 @@ class Bookpage extends StatefulWidget {
 
 class _BookpageState extends State<Bookpage> {
   String sem = '';
+  bool firstSelected = true;
 
   void semname() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -31,16 +32,85 @@ class _BookpageState extends State<Bookpage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: AppColors.primary,
         title: Text(
           sem,
           style: GoogleFonts.plusJakartaSans(
             color: AppColors.secondary,
             fontWeight: FontWeight.bold,
             fontSize: 24,
-            
           ),
         ),
         centerTitle: true,
+      ),
+      body: Container(
+        height: 100,
+        decoration: BoxDecoration(
+          border: Border(
+            bottom: BorderSide(color: AppColors.secondary, width: 2),
+          ),
+          color: AppColors.primary,
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(30),
+            bottomRight: Radius.circular(30),
+          ),
+        ),
+        child: Stack(
+          children: [
+            Positioned(
+              bottom: 20,
+              left: 20,
+              child: Row(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        firstSelected = true;
+                      });
+                    },
+
+                    child: Text(
+                      'Books',
+                      style: firstSelected
+                          ? GoogleFonts.plusJakartaSans(
+                              color: AppColors.secondary,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            )
+                          : GoogleFonts.plusJakartaSans(
+                              color: AppColors.secondary.withOpacity(0.6),
+                              fontWeight: FontWeight.normal,
+                              fontSize: 20,
+                            ),
+                    ),
+                  ),
+                  SizedBox(width: 20),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        firstSelected = false;
+                      });
+                    },
+                    child: Text(
+                      'Question Papers',
+                      style: firstSelected
+                          ? GoogleFonts.plusJakartaSans(
+                              color: AppColors.secondary.withOpacity(0.6),
+                              fontWeight: FontWeight.normal,
+                              fontSize: 20,
+                            )
+                          : GoogleFonts.plusJakartaSans(
+                              color: AppColors.secondary,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
